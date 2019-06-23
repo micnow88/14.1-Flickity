feefb9daeeffd300d7b68881eac699add79f8296
Michał Nowakowski <nowakowskimichal1988@gmail.com>
2019-06-23 10:43:02 +0200

    Add maps and marker

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 'use strict';
- 
+
+//zmienne dla Mustache 
 var templateSlide = document.getElementById('template-slide').innerHTML;
+
 Mustache.parse(templateSlide);
 
 var listSlide = '';
@@ -8,15 +10,13 @@ var listSlide = '';
 var resultCell = document.getElementsByClassName('carousel-cell');
 
 for(var i = 0; i < cellData.length; i++) {
-    console.log(cellData);
-    listSlide = Mustache.render(templateSlide, cellData[i]);
-    resultCell[i].insertAdjacentHTML('beforeend', listSlide);
-  }
+  listSlide = Mustache.render(templateSlide, cellData[i]);
+  resultCell[i].insertAdjacentHTML('beforeend', listSlide);
+}
 
-
+//Carousel
 var mainCarousel = document.querySelector('.main-carousel');
 var flkty = new Flickity( mainCarousel, {
-  // options
   cellAlign: 'center',
   contain: true,
   wrapAround: true,
@@ -27,13 +27,11 @@ var flkty = new Flickity( mainCarousel, {
   initialIndex: 1
 });
 
-// element argument can be a selector string
-//   for an individual element
 var flkty = new Flickity( '.main-carousel', {
-  // options
   groupCells: true
 });
 
+//Reset button
 var buttonReset = document.getElementById('reset-button');
 var initialIndex = document.getElementById('carousel-cell1');
 
@@ -41,9 +39,36 @@ buttonReset.addEventListener( 'click', function( event ) {
   flkty.selectCell(initialIndex);
 });
 
+//Progress bar
 var progressBar = document.querySelector('.progress-bar')
 
 flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+//Mapy
+window.initMap = function() {
+  var rynek = {lat: 50.678995, lng: 21.749001};
+  
+  //Pozycjonowanie i ustawienia mapy
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 5,
+    center: rynek
+  });
+    
+  //Marker początkowy
+  var marker = new google.maps.Marker({
+    position: rynek,
+    map: map
+  });
+
+  //Pętla dla markerów dla slajdów
+  for (var i=0; i < cellData.length; i++) {
+    var markerData = cellData[i].cords;
+    var markerCell = new google.maps.Marker({
+      position: markerData,
+      map: map
+  });
+  }
+}
